452a31cf820ba1a831abcfb971c778e6449e017a
JDaelfc <jfloreschoque.unsa.edu.pe>
2023-05-04 13:01:40 -0500

    bienvenida

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,11 @@
 public class HolaMundo {
     public static void main(String args[]) {
+        // Hola mundo
         System.out.println ("¡Hola mundo!");
+
+        // Segundo Commit
+        System.out.println("Bienvenida al curso!");
+
 
     }
 }
